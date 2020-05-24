ceccb579afa1f0785f5d49de8b3fbb229b5b29d8
Justyna Opalinska <ju.opalinska@gmail.com>
2020-05-24 19:04:01 +0200

    parametrized tests

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,11 +1,16 @@
 public class LeapYear {
     public boolean leapYear(int year) {
-        if ((year % 4) == 0) {
+        if ((year % 4) == 0 && (year % 100) == 0 && (year % 400) == 0) {
             System.out.println("YES! This is leap year");
             return true;
 
-        } else if ((year % 100) != 0) {
+        } else if ((year % 4) == 0 &&(year % 100) == 0) {
+            System.out.println("Ohh...this is not a leap year...");
             return false;
+
+        } else if ((year % 4) == 0) {
+            System.out.println("YES! This is leap year");
+            return true;
 
         } else {
             System.out.println("Ohh...this is not a leap year...");
--- kodilla-intro/src/test/java/LeapYearTestSuite.java
+++ kodilla-intro/src/test/java/LeapYearTestSuite.java
@@ -1,14 +1,33 @@
 import org.junit.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.ParameterizedTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class LeapYearTestSuite {
+
+    LeapYear year = new LeapYear();
 
     @Test
     public void testLeapYear(){
-        LeapYear leapYear = new LeapYear();
-        boolean result = leapYear.leapYear(2020);
-        boolean result1 = leapYear.leapYear(2019);
+
+        boolean result = year.leapYear(2020);
+        boolean result1 = year.leapYear(2019);
         assertTrue(result);
         assertFalse(result1);
 
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {2020, 2016, 2012, 2008, 2004, 2000, 1996, 0, 4, 16})
+    public void shouldReturnTrueForLeapYears(int years){
+        boolean result = year.leapYear(years);
+        assertTrue(result);
+
+    }
+    @ParameterizedTest
+    @ValueSource(ints ={2019, 2, -3, -11, 99, 5, 2541, 18, 2017})
+    public void shouldReturnFalseForLeapYears(int number){
+        boolean result1 = year.leapYear(number);
+        assertFalse(result1);
     }
 }
